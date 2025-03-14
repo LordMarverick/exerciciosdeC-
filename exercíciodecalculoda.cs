38c6e5052265a435758b9dd7381519e240bd61df
Marcus Vinicius <marcusvinicius.rj45@gmail.com>
2025-03-14 15:14:13 -0300

    Add files via upload

---
+++ exercíciodecalculoda.cs
@@ -0,0 +1,17 @@
+//codigo com a função de calculo e conversão// 
+
+console.writeline("Digite a altura do retânglo?");
+float altura = convert.toint 32 (console.readline());
+
+console.writeline ("Digite a base do retângulo?");
+float base = converto.toint 32 (console.readline());
+
+
+double total = altura + base;
+float = total =0;
+
+double votos altura = (altura/retânglo)*100;
+double votos nulos = (base/retânglo)*100;
+
+contel.writeline ("total o retângulo:"+total);
+console.Readkey();
