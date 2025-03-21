5dc4de92c30c3c48917a5cc61b6c9e1fcb027deb
Marcus Vinicius <marcusvinicius.rj45@gmail.com>
2025-03-21 18:07:55 -0300

    Update exercíciodecalculoda.cs

--- exercíciodecalculoda.cs
+++ exercíciodecalculoda.cs
@@ -1,17 +1,16 @@
 //codigo com a função de calculo e conversão// 
+static void main (string[]args){
+
+double A, B, somatotal;
 
 console.writeline("Digite a altura do retânglo?");
-float altura = convert.toint 32 (console.readline());
+double A = convert.double(console.readline());
 
 console.writeline ("Digite a base do retângulo?");
-float base = converto.toint 32 (console.readline());
+double B = converto.doble(console.readline());
 
+somatotal = A + B;
 
-double total = altura + base;
-float = total =0;
-
-double votos altura = (altura/retânglo)*100;
-double votos nulos = (base/retânglo)*100;
-
-contel.writeline ("total o retângulo:"+total);
+contel.writeline ("total o retângulo:"+somatotal);
 console.Readkey();
+}
